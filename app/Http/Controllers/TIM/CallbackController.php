345b6b5a496a44601e7183008766bea17b6e10e6
hyb <804141892@qq.com>
2020-05-23 11:02:34 +0800

    save

--- app/Http/Controllers/TIM/CallbackController.php
+++ app/Http/Controllers/TIM/CallbackController.php
@@ -11,6 +11,10 @@ use Illuminate\Support\Facades\Log;
 class CallbackController extends Controller
 {
 
+    protected $commands = [
+        'C2C.CallbackBeforeSendMsg' => 'beforeSendMsg',
+        'Group.CallbackBeforeSendMsg' => 'groupBeforeSendMsg',
+    ];
 
     public function callback(Request $request){
 
@@ -18,19 +22,33 @@ class CallbackController extends Controller
 
         Log::debug('callback data',$data);
 
-        if(!empty($data) && isset($data['CallbackCommand'])){
-            $count = 1;
-            $method = str_replace('C2C.Callback','',$data['CallbackCommand'],$count);
-            $method = lcfirst($method);
-            if (method_exists($this, $method)) {
-                return $this->$method($request);
-            }
+        if(!empty($data)
+            && isset($data['CallbackCommand'])
+            && isset($this->commands[$data['CallbackCommand']])
+            && !empty($this->commands[$data['CallbackCommand']]))
+        {
+            return call_user_func([$this,$this->commands[$data['CallbackCommand']]],$request);
         }
         return response()->json(['ActionStatus'=>'OK','ErrorInfo'=>'','ErrorCode'=>0]);
     }
 
 
     public function beforeSendMsg(Request $request){
+        //$data = $request->post();
+        return response()->json([
+            'ActionStatus'=>'OK','ErrorInfo'=>'','ErrorCode'=>0,
+            'MsgBody'=>[
+                [
+                    'MsgType'=>'TIMTextElem',
+                    'MsgContent'=>[
+                        'Text'=>'red packet'
+                    ]
+                ]
+            ]
+        ]);
+    }
+
+    public function groupBeforeSendMsg(Request $request){
         //$data = $request->post();
         return response()->json([
             'ActionStatus'=>'OK','ErrorInfo'=>'','ErrorCode'=>0,
