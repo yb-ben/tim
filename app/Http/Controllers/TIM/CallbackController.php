886383db911705779dbc345446e56e5a13e20ab0
hyb <804141892@qq.com>
2020-05-23 10:50:57 +0800

    save

--- app/Http/Controllers/TIM/CallbackController.php
+++ app/Http/Controllers/TIM/CallbackController.php
@@ -15,7 +15,31 @@ class CallbackController extends Controller
     public function callback(Request $request){
 
         $data = $request->post();
+
         Log::debug('callback data',$data);
+
+        if(!empty($data) && isset($data['CallbackCommand'])){
+            $count = 1;
+            $method = str_replace('C2C.Callback','',$data['CallbackCommand'],$count);
+            $method = lcfirst($method);
+            return $this->$method($request);
+        }
         return response()->json(['ActionStatus'=>'OK','ErrorInfo'=>'','ErrorCode'=>0]);
+    }
+
+
+    public function beforeSendMsg(Request $request){
+        //$data = $request->post();
+        return response()->json([
+            'ActionStatus'=>'OK','ErrorInfo'=>'','ErrorCode'=>0,
+            'MsgBody'=>[
+                [
+                    'MsgType'=>'TIMTextElem',
+                    'MsgContent'=>[
+                        'Text'=>'red packet'
+                    ]
+                ]
+            ]
+        ]);
     }
 }
